eb226b4e3da29a7efe6eb67ad10d1995cb7ac956
vot5kor <vot5kor@gmail.com>
2025-02-27 19:12:46 +0530

    vpc module added

---
+++ main.tf
@@ -0,0 +1,48 @@
+resource "aws_instance" "This"{
+    ami     = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    vpc_security_group_ids  = [aws_security_group.allow_docker.id]
+   
+    #20gb not enough
+    root_block_device {
+        volume_size = 50 # set root  volume size to 50
+        volume_type = "gp3" # use gp3 for better performance(optional)
+
+    }
+    #user_data = file(docker.sh)
+
+    tags = {
+      Name = "docker"
+    }
+}
+
+
+resource "aws_security_group" "allow_docker" {
+  name              = "allow_docker"
+  description       = "allow tls inbound trafic and all outbound traffic"
+ 
+ ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+ }
+
+ ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+ }
+ 
+ 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow_tls"
+  }
+}
